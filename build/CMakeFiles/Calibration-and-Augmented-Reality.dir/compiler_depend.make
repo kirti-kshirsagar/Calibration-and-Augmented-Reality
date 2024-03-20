162b421af7dbd2d653793c81a1211b50829fb149
juttu-s <156356919+juttu-s@users.noreply.github.com>
2024-03-19 20:01:59 -0400

    Delete build directory

--- build/CMakeFiles/Calibration-and-Augmented-Reality.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for Calibration-and-Augmented-Reality.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/sakiran/Internal/2nd Semester/PRCV/Calibration-and-Augmented-Reality
-# Build directory: /media/sakiran/Internal/2nd Semester/PRCV/Calibration-and-Augmented-Reality/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
